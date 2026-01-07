bd0f6df00b85ba56c6ea5f083546e0e25ea0608f
Adina Surya Siva Veera Bhadra Rao <008785446@coyote.csusb.edu>
2026-01-07 19:08:44 +0530

    adding data for RAG Indexing

--- src/search.py
+++ src/search.py
@@ -14,7 +14,7 @@ class RAGSearch:
         faiss_path = os.path.join(persist_dir, "faiss.index")
         meta_path = os.path.join(persist_dir, "metadata.pkl")
         if not (os.path.exists(faiss_path) and os.path.exists(meta_path)):
-            from data_loader import load_all_documents
+            from src.data_loader import load_all_documents
             docs = load_all_documents("data")
             self.vectorstore.build_from_documents(docs)
         else:
